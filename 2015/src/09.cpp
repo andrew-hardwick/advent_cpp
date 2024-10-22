e51cf6b2572424d0deafc25fc9251f1f16d3bed9
Andrew Hardwick <drew.hardwick@gmail.com>
2024-10-22 08:51:57 -0500

    permutations written and 2015-09 complete

--- 2015/src/09.cpp
+++ 2015/src/09.cpp
@@ -1,11 +1,13 @@
 #include "09.h"
 
 #include <algorithm>
-#include <iostream>
+#include <limits>
 #include <map>
+#include <string>
 #include <utility>
 #include <vector>
 
+#include "combinatorics.h"
 #include "fileOps.h"
 #include "stringUtils.h"
 
@@ -25,7 +27,7 @@ namespace advent { namespace y2015 { namespace d09
 		return { splitLine.at(0), splitLine.at(1), std::stoi(splitLine.at(2)) };
 	}
 
-	std::tuple<std::map<std::pair<std::string, std::string>, std::size_t>, std::vector<std::string>> loadInput(
+	std::tuple<std::map<std::pair<std::size_t, std::size_t>, std::size_t>, std::size_t> loadInput(
 			const std::string& inFilename)
 	{
 		std::map<std::pair<std::string, std::string>, std::size_t> distances;
@@ -51,29 +53,77 @@ namespace advent { namespace y2015 { namespace d09
 			}
 		}
 
-		return { distances, locations };
+		std::map<std::string, std::size_t> locationIndices;
+
+		for (std::size_t i = 0; i < locations.size(); i++)
+		{
+			locationIndices.emplace(std::make_pair(locations.at(i), i));
+		}
+
+		std::map<std::pair<std::size_t, std::size_t>, std::size_t> resultDistances;
+
+		for (const auto& [ locationPair, distance ] : distances)
+		{
+			const auto& [ locationOne, locationTwo ] = locationPair;
+
+			std::pair<std::size_t, std::size_t> indicesPair { locationIndices.at(locationOne), locationIndices.at(locationTwo) };
+
+			resultDistances.emplace(std::make_pair(indicesPair, distance));
+		}
+
+		return { resultDistances, locations.size() };
 	}
 
 	std::string partOne(
 			const std::string& inFilename)
 	{
-		auto [ distances, locations ] = loadInput(inFilename);
+		auto [ distances, locationCount ] = loadInput(inFilename);
 
-		for (const auto& [ locs, distance ] : distances)
+		auto perms = utility::combinatorics::permutations(locationCount);
+
+		auto minDistance = std::numeric_limits<std::size_t>::max();
+
+		for (const auto& row : perms)
 		{
-			const auto& [ loc1, loc2 ] = locs;
+			std::size_t distance = 0;
 
-			std::cout << loc1 << " " << loc2 << " " << distance << "\n";
+			for (std::size_t i = 0; i < row.size() - 1; i++)
+			{
+				std::pair<std::size_t, std::size_t> entry { row.at(i), row.at(i + 1) };
+				
+				distance += distances.at(entry);
+			}
+
+			minDistance = minDistance > distance ? distance : minDistance;
 		}
-			
 
-		return "nonimpl";
+		return std::to_string(minDistance);
 	}
 
 	std::string partTwo(
 			const std::string& inFilename)
 	{
-		return "nonimpl";
+		auto [ distances, locationCount ] = loadInput(inFilename);
+
+		auto perms = utility::combinatorics::permutations(locationCount);
+
+		auto maxDistance = std::numeric_limits<std::size_t>::min();
+
+		for (const auto& row : perms)
+		{
+			std::size_t distance = 0;
+
+			for (std::size_t i = 0; i < row.size() - 1; i++)
+			{
+				std::pair<std::size_t, std::size_t> entry { row.at(i), row.at(i + 1) };
+				
+				distance += distances.at(entry);
+			}
+
+			maxDistance = maxDistance < distance ? distance : maxDistance;
+		}
+
+		return std::to_string(maxDistance);
 	}
 
 	std::pair<adventFunctor, adventFunctor> getParts()
