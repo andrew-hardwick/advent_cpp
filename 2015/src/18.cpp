9860d3f9bf99e78965a7f3a71d07ad70790f6caa
Drew Hardwick <drew.hardwick@gmail.com>
2024-12-02 11:11:23 -0600

    2015 d18 complete

--- 2015/src/18.cpp
+++ 2015/src/18.cpp
@@ -5,11 +5,10 @@
 #include <algorithm>
 #include <cstddef>
 #include <iostream>
-#include <iterator>
 #include <stdexcept>
 #include <string>
 
-#define STEP_COUNT 4
+#define STEP_COUNT 100
 
 
 namespace advent { namespace y2015 { namespace d18
@@ -68,42 +67,111 @@ namespace advent { namespace y2015 { namespace d18
 	{
 		std::size_t result = 0;
 
+		const auto& [ maxY, maxX ] = extents;
+		const auto& [ curY, curX ] = location;
+
+		if (curY > 0)
+		{
+			if (curX > 0)
+				if (map[curY - 1][curX - 1])
+					result++;
+			if (curX < maxX - 1)
+				if (map[curY - 1][curX + 1])
+					result++;
+			if (map[curY - 1][curX]) result++;
+		}
+		if (curY < maxY - 1)
+		{
+			if (curX > 0)
+				if (map[curY + 1][curX - 1])
+					result++;
+			if (curX < maxX - 1)
+				if (map[curY + 1][curX + 1])
+					result++;
+			if (map[curY + 1][curX]) result++;
+		}
+		if (curX > 0)
+			if (map[curY][curX - 1]) result++;
+		if (curX < maxX - 1)
+			if (map[curY][curX + 1]) result++;
 
 		return result;
 	}
 
-	void step(
+	void lightCorners(
 			std::vector<std::vector<bool>>& map,
+			const std::tuple<std::size_t, std::size_t>& extents)
+	{
+		const auto& [ height, width ] = extents;
+
+		map[0][0] = true;
+		map[0][width - 1] = true;
+		map[height - 1][0] = true;
+		map[height - 1][width - 1] = true;
+	}
+
+	std::vector<std::vector<bool>> step(
+			const std::vector<std::vector<bool>>& map,
 			const std::tuple<std::size_t, std::size_t>& extents,
 			bool cornersLit)
 	{
 		const auto& [ height, width ] = extents;
 
+		std::vector<std::vector<bool>> result;
+
 		for (std::size_t y = 0; y < height; y++)
 		{
+			std::vector<bool> resultRow;
+
 			for (std::size_t x = 0; x < width; x++)
 			{
 				auto liveNeighbors = countLiveNeighbors(map, extents, { y, x });
 
 				auto current = map.at(y).at(x);
 
-				if (current && liveNeighbors != 2 && liveNeighbors != 3)
-					map.at(y).at(x) = false;
-				if (!current && liveNeighbors == 3)
-					map.at(y).at(x) = true;
+				if (current && !(liveNeighbors == 2 || liveNeighbors == 3))
+					current = false;
+				else if (!current && liveNeighbors == 3)
+					current = true;
+
+				resultRow.push_back(current);
+			}
+
+			result.push_back(resultRow);
+		}
+
+		if (cornersLit)
+		{
+			lightCorners(result, extents);
+		}
+
+		return result;
+	}
+
+	std::size_t countLit(
+			const std::vector<std::vector<bool>>& map)
+	{
+		std::size_t result = 0;
+
+		for (const auto& row : map)
+		{
+			for (const auto& val : row)
+			{
+				if (val) result++;
 			}
 		}
+
+		return result;
 	}
 
 	void printMap(
 			const std::vector<std::vector<bool>>& map)
 	{
-		std::cout << "\n";
 		for (const auto& row : map)
 		{
-			for (const auto& v : row)
+			for (const auto& val : row)
 			{
-				if (v)
+				if (val)
 					std::cout << "#";
 				else
 					std::cout << ".";
@@ -113,10 +181,26 @@ namespace advent { namespace y2015 { namespace d18
 		std::cout << "\n";
 	}
 
-	std::size_t countLit(
-			const std::vector<std::vector<bool>>& map)
+	void printLiveNeighbors(
+			const std::vector<std::vector<bool>>& map,
+			const std::tuple<std::size_t, std::size_t>& extents)
 	{
-		return 0;
+		const auto& [ maxY, maxX ] = extents;
+
+		for (std::size_t y = 0; y < maxY; y++)
+		{
+			for (std::size_t x = 0; x < maxX; x++)
+			{
+				auto live = countLiveNeighbors(map, extents, { y, x });
+				
+				if (live == 0)
+					std::cout << '.';
+				else
+					std::cout << live;
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
 	}
 
 	std::string partOne(
@@ -126,13 +210,8 @@ namespace advent { namespace y2015 { namespace d18
 
 		auto extents = getExtents(map);
 
-		printMap(map);
-
 		for (std::size_t i = 0; i < STEP_COUNT; i++)
-		{
-			step(map, extents, false);
-			printMap(map);
-		}
+			map = step(map, extents, false);
 
 		auto result = countLit(map);
 
@@ -142,7 +221,18 @@ namespace advent { namespace y2015 { namespace d18
 	std::string partTwo(
 			const std::string& inFilename)
 	{
-		return "nonimpl";
+		auto map = loadInput(inFilename);
+
+		auto extents = getExtents(map);
+
+		lightCorners(map, extents);
+
+		for (std::size_t i = 0; i < STEP_COUNT; i++)
+			map = step(map, extents, true);
+
+		auto result = countLit(map);
+
+		return std::to_string(result);
 	}
 
 	std::pair<adventFunctor, adventFunctor> getParts()
